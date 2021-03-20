1a0608dd07c13762fbcd6926b4f62573ba3c411c
Brian White <brian@thirdember.com>
2021-03-20 11:28:28 -0400

    show svcmenu after command finishes

--- svcmenu.py
+++ svcmenu.py
@@ -2,25 +2,35 @@
 
 from simple_term_menu import TerminalMenu
 import os
+import signal
 
 SERVICE_NAME = "beltlight"
 
-def main():
-    print(f"{SERVICE_NAME} service commands...")
-    
-    terminal_menu = TerminalMenu(["stop", "restart", "logs", "start", "status"])
-    menu_entry_index = terminal_menu.show()
+class exit_monitor_setup:
+    exit_now_flag_raised = False
 
-    if menu_entry_index == 0:
-        os.system(f"sudo systemctl stop {SERVICE_NAME}")
-    elif menu_entry_index == 1:
-        os.system(f"sudo systemctl restart {SERVICE_NAME}")
-    elif menu_entry_index == 2:
-        os.system(f"journalctl -u {SERVICE_NAME} -f")
-    elif menu_entry_index == 3:
-        os.system(f"sudo systemctl start {SERVICE_NAME}")
-    elif menu_entry_index == 4:
-        os.system(f"systemctl status {SERVICE_NAME}")
+    def __init__(self):
+        signal.signal(signal.SIGINT, self.exit_gracefully)
+        signal.signal(signal.SIGTERM, self.exit_gracefully)
+
+    def exit_gracefully(self, signum, frame):
+        self.exit_now_flag_raised = True
+
 
 if __name__ == "__main__":
-    main()
+    exit_monitor = exit_monitor_setup()
+
+    while not exit_monitor.exit_now_flag_raised:
+        terminal_menu = TerminalMenu(["stop", "restart", "logs", "start", "status"])
+        menu_entry_index = terminal_menu.show()
+
+        if menu_entry_index == 0:
+            os.system(f"sudo systemctl stop {SERVICE_NAME}")
+        elif menu_entry_index == 1:
+            os.system(f"sudo systemctl restart {SERVICE_NAME}")
+        elif menu_entry_index == 2:
+            os.system(f"journalctl -u {SERVICE_NAME} -f")
+        elif menu_entry_index == 3:
+            os.system(f"sudo systemctl start {SERVICE_NAME}")
+        elif menu_entry_index == 4:
+            os.system(f"systemctl status {SERVICE_NAME}")
